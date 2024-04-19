f61767c4ffb7d283387ac6e9f728fce0c97483fd
Anton Gorbovyi <anton.gorbovyi@gmail.com>
2024-04-19 21:30:29 +0200

    LibraryApp v.2.5, ui

--- src/main/java/librarymanagementapp/service/util/UserInput.java
+++ src/main/java/librarymanagementapp/service/util/UserInput.java
@@ -10,12 +10,12 @@ public class UserInput {
     private static Scanner scanner = new Scanner(System.in);
 
     public static String getText(String message) {
-        System.out.println(message);
+        System.out.print(message);
         return scanner.nextLine();
     }
 
     public static Integer getInt(String message) {
-        System.out.println(message);
+        System.out.print(message);
         return scanner.nextInt();
     }
 }
