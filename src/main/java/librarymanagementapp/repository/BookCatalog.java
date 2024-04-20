c582f16085d03494b32f69ec999ef6f2be01dc3f
HalynaPoryvaieva <lastiklastik407@gmail.com>
2024-04-20 13:39:58 +0200

    Fix 3 final :-)

--- src/main/java/librarymanagementapp/repository/BookCatalog.java
+++ src/main/java/librarymanagementapp/repository/BookCatalog.java
@@ -30,15 +30,15 @@ public class BookCatalog {
     public static void main(String[] args) {
         BookCatalog catalog = new BookCatalog();
 
-        catalog.addBook(new Book("Taras Shevchenko", "Kobzar", "Poetry", "Kyiv Old Guard", 1, true));
-        catalog.addBook(new Book("Ivan Franko", "Za dvoma zaitsiamy", "Drama", "Lviv Printing House", 2, true));
-        catalog.addBook(new Book("Lesia Ukrainka", "Lisova pisnia", "Poetry", "Kyiv Old Guard", 3, true));
-        catalog.addBook(new Book("Ivan Nechuy-Levytsky", "Pіznorid", "Novel", "Kyiv Old Guard", 4, true));
-        catalog.addBook(new Book("Mykhailo Kotsiubynsky", "Intermezzo", "Novel", "Kyiv Old Guard", 5, true));
-        catalog.addBook(new Book("Panas Myrny", "Pіsnі smutku", "Novel", "Lviv Printing House", 6, true));
-        catalog.addBook(new Book("Marko Vovchok", "Novellas", "Novel", "Lviv Printing House", 7, true));
-        catalog.addBook(new Book("Ivan Franko", "Pіsnі smіlі", "Poetry", "Kyiv Old Guard", 8, true));
-        catalog.addBook(new Book("Lesia Ukrainka", "Kaminnyi hospodar", "Drama", "Lviv Printing House", 9, true));
-        catalog.addBook(new Book("Pavlo Tychyna", "Pluh", "Poetry", "Kyiv Old Guard", 10, true));
+        catalog.addBook(new Book("Taras Shevchenko", "Kobzar", "Poetry", "Kyiv Old Guard", 1));
+        catalog.addBook(new Book("Ivan Franko", "Za dvoma zaitsiamy", "Drama", "Lviv Printing House", 2));
+        catalog.addBook(new Book("Lesia Ukrainka", "Lisova pisnia", "Poetry", "Kyiv Old Guard", 3));
+        catalog.addBook(new Book("Ivan Nechuy-Levytsky", "Pіznorid", "Novel", "Kyiv Old Guard", 4));
+        catalog.addBook(new Book("Mykhailo Kotsiubynsky", "Intermezzo", "Novel", "Kyiv Old Guard", 5));
+        catalog.addBook(new Book("Panas Myrny", "Pіsnі smutku", "Novel", "Lviv Printing House", 6));
+        catalog.addBook(new Book("Marko Vovchok", "Novellas", "Novel", "Lviv Printing House", 7));
+        catalog.addBook(new Book("Ivan Franko", "Pіsnі smіlі", "Poetry", "Kyiv Old Guard", 8));
+        catalog.addBook(new Book("Lesia Ukrainka", "Kaminnyi hospodar", "Drama", "Lviv Printing House", 9));
+        catalog.addBook(new Book("Pavlo Tychyna", "Pluh", "Poetry", "Kyiv Old Guard", 10));
     }
 }
